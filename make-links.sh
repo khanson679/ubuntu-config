ca8b637cfb1b237ba1bd4dcab3d6b249d46933d7
Kenneth Hanson <khanson679@gmail.com>
2018-05-24 21:31:50 +0900

    Add check for file at destination, with prompt to remove.

--- make-links.sh
+++ make-links.sh
@@ -2,6 +2,9 @@
 # make links to all given files in home folder, adding a preceding dot
 
 for file in $@; do
-	echo $file
-	ln -s $file $HOME/.$(basename $file);
+    original=$(realpath $   file)
+    link=$HOME/.$(basename $file)
+	echo "Linking $link -> $original"
+	if [ -f $link ]; then rm -i $link; fi
+	ln -s $original $link
 done
