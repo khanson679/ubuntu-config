e6e00137f3cbd08592a51e186c0b590d1f2cc2f8
Kenneth Hanson <khanson679@gmail.com>
2022-07-15 00:03:16 -0400

    Update everything. Remove duplicates.

--- make-links.sh
+++ make-links.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # make links to all given files in home folder, adding a preceding dot
-
-for file in $@; do
+set -x
+for file in dotfiles/*; do
     original=$(realpath $file)
     link=$HOME/.$(basename $file)
 	echo "Linking $link -> $original"
 	if [ -f $link ]; then rm -i $link; fi
-	ln -s $original $link
+	ln -fs $original $link
 done
